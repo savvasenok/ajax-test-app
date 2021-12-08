cc1c38a8bb15deca3a2fafb3debbe2a0c40e7974
savvasenok <nightowlsavva@gmail.com>
2021-12-08 15:34:10 +0200

    ATA004 - added navigation component to app

---
+++ app/src/main/java/xyz/savvamirzoyan/share/ajaxtest/ContactsListFragment.kt
@@ -0,0 +1,18 @@
+package xyz.savvamirzoyan.share.ajaxtest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ContactsListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_contacts_list, container, false)
+    }
+}
